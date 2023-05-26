f1683d2b98bc64b5e548c5eb6cf4c80d5f52a3ad
Joe-Moussally <joemoussally111@gmail.com>
2023-05-26 03:46:50 +0300

    [General] Arranging image order

--- App.js
+++ App.js
@@ -11,16 +11,16 @@ import {
 } from "react-native"
 
 const images = [
-  require("./images/image-0.jpeg"),
-  require("./images/image-1.jpeg"),
-  require("./images/image-2.jpeg"),
-  require("./images/image-3.jpeg"),
-  require("./images/image-4.webp"),
   require("./images/image-5.jpeg"),
-  require("./images/image-6.jpeg"),
-  require("./images/image-7.png"),
   require("./images/image-8.jpeg"),
-  require("./images/image-9.jpeg")
+  require("./images/image-3.jpeg"),
+  require("./images/image-2.jpeg"),
+  require("./images/image-7.png"),
+  require("./images/image-4.webp"),
+  require("./images/image-6.jpeg"),
+  require("./images/image-1.jpeg"),
+  require("./images/image-9.jpeg"),
+  require("./images/image-0.jpeg")
 ]
 
 const IMAGE_THUMBNAIL_SIZE = 82
